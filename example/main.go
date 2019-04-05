7ef6fc174285c3227175f013ee1bae0acf3c299c
Johandry Amador <johandry@gmail.com>
2019-04-05 00:11:17 -0700

    fix and improve example

--- example/main.go
+++ example/main.go
@@ -1,44 +1,59 @@
 package main
 
 import (
-  "log"
-  "strconv"
+	"flag"
+	"log"
+	"strconv"
 
-  "github.com/hashicorp/terraform/builtin/provisioners/file"
-  "github.com/johandry/terranova"
-  "github.com/terraform-providers/terraform-provider-aws/aws"
+	"github.com/hashicorp/terraform/builtin/provisioners/file"
+	"github.com/johandry/terranova"
+	"github.com/terraform-providers/terraform-provider-aws/aws"
 )
 
-var code string
+var (
+	code    string
+	count   int
+	keyName string
+)
 
 const stateFilename = "aws-ec2-ubuntu.tfstate"
 
 func main() {
-  count := 1
-  keyName := "username"
+	flag.Parse()
 
-	platform := terranova.NewPlatform(code)
-							.AddProvider("aws", aws.Provider())
-							.AddProvisioner("file", file.Provisioner())
-							.Var("count", strconv.Itoa(count))
-							.Var("key_name", keyName)
-  
-  if _, err := platform.ReadStateFile(stateFilename); err != nil {
-    log.Panicf("Fail to load the initial state of the platform from file %s. %s", stateFilename, err)
-  }
+	if len(keyName) == 0 {
+		log.Fatalf("key name is required. Create or use an existing key name assigned to your AWS account")
+	}
 
-  terminate := (count == 0)
-  if err := platform.Apply(terminate); err != nil {
-    log.Fatalf("Fail to apply the changes to the platform. %s", err)
-  }
+	if count < 0 {
+		log.Fatalf("count cannot be negative. It has to be '0' to terminate all the creted instances or the desired number of instances")
+	}
 
-  if err := platform.WriteStateFile(stateFilename); err != nil {
-    log.Fatalf("Fail to save the final state of the platform to file %s. %s", stateFilename, err)
-  }
+	platform := terranova.NewPlatform(code).
+		AddProvider("aws", aws.Provider()).
+		AddProvisioner("file", file.Provisioner()).
+		Var("count", strconv.Itoa(count)).
+		Var("key_name", keyName).
+		ReadStateFromFile(stateFilename)
+
+	if err != nil {
+		log.Fatalf("Fail to load the initial state of the platform from file %s. %s", stateFilename, err)
+	}
+
+	if err := platform.Apply((count == 0)); err != nil {
+		log.Fatalf("Fail to apply the changes to the platform. %s", err)
+	}
+
+	if err := platform.WriteStateToFile(stateFilename); err != nil {
+		log.Fatalf("Fail to save the final state of the platform to file %s. %s", stateFilename, err)
+	}
 }
 
 func init() {
-  code = `
+	flag.IntVar(&count, "count", 2, "number of instances to create. Set to '0' to terminate them all.")
+	flag.StringVar(&keyName, "keyname", "", "keyname to access the instances.")
+
+	code = `
   variable "count" 		{ default = 2 }
   variable "key_name" {}
   provider "aws" {
