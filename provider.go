0a0a6b120c60a76e279d4a320e67da3a7132f281
Johandry Amador <johandry@gmail.com>
2019-10-16 15:17:33 -0700

    add license to the header

--- provider.go
+++ provider.go
@@ -1,3 +1,19 @@
+/*
+Copyright The Terranova Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package terranova
 
 import (
@@ -38,6 +54,11 @@ func NewProvider(provider terraform.ResourceProvider) *Provider {
 	return &Provider{
 		provider: sp,
 	}
+}
+
+func providersFactory(rp terraform.ResourceProvider) providers.Factory {
+	p := NewProvider(rp)
+	return providers.FactoryFixed(p)
 }
 
 // GetSchema implements the GetSchema from providers.Interface. Returns the
