2ff3a72c00e24ed8d54784a833f5dc33609efd25
Johandry Amador <johandry@gmail.com>
2020-01-06 15:46:00 -0800

    test for NewPlatform

--- platform_test.go
+++ platform_test.go
@@ -19,7 +19,85 @@ package terranova
 import (
 	"reflect"
 	"testing"
+
+	"github.com/hashicorp/terraform/providers"
+	"github.com/hashicorp/terraform/provisioners"
+	"github.com/hashicorp/terraform/states"
+	"github.com/hashicorp/terraform/terraform"
+	"github.com/terraform-providers/terraform-provider-null/null"
 )
+
+func TestNewPlatform(t *testing.T) {
+	type args struct {
+		code  string
+		hooks []terraform.Hook
+	}
+	tests := []struct {
+		name string
+		args args
+		want *Platform
+	}{
+		{"default values", args{"fake code", []terraform.Hook{}}, &Platform{
+			Code: "fake code",
+			Providers: map[string]providers.Factory{
+				"null": providers.FactoryFixed(NewProvider(null.Provider())),
+			},
+			Provisioners:  map[string]provisioners.Factory{},
+			Vars:          nil,
+			State:         states.NewState(),
+			Hooks:         []terraform.Hook{},
+			LogMiddleware: nil,
+		}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := NewPlatform(tt.args.code, tt.args.hooks...); !reflect.DeepEqual(got, tt.want) {
+				// t.Errorf("NewPlatform() = %+v, want %+v", got, tt.want)
+				if !reflect.DeepEqual(got.Code, tt.want.Code) {
+					t.Errorf("NewPlatform().Code = %+v, want %+v", got.Code, tt.want.Code)
+				}
+				if !reflect.DeepEqual(got.Providers, tt.want.Providers) {
+					for name := range got.Providers {
+						if _, ok := tt.want.Providers[name]; !ok {
+							t.Errorf("NewPlatform().Providers[%s] not found in wanted", name)
+						}
+					}
+					for name := range tt.want.Providers {
+						if _, ok := got.Providers[name]; !ok {
+							t.Errorf("NewPlatform().Providers[%s] not found", name)
+						}
+					}
+					// t.Errorf("NewPlatform().Providers = \n'%#v'\n, want \n'%#v'", got.Providers, tt.want.Providers)
+				}
+				if !reflect.DeepEqual(got.Provisioners, tt.want.Provisioners) {
+					for name := range got.Provisioners {
+						if _, ok := tt.want.Provisioners[name]; !ok {
+							t.Errorf("NewPlatform().Provisioners[%s] not found in wanted", name)
+						}
+					}
+					for name := range tt.want.Provisioners {
+						if _, ok := got.Provisioners[name]; !ok {
+							t.Errorf("NewPlatform().Provisioners[%s] not found", name)
+						}
+					}
+					// t.Errorf("NewPlatform().Provisioners = %+v, want %+v", got.Provisioners, tt.want.Provisioners)
+				}
+				if !reflect.DeepEqual(got.Vars, tt.want.Vars) {
+					t.Errorf("NewPlatform().Vars = %+v, want %+v", got.Vars, tt.want.Vars)
+				}
+				if !reflect.DeepEqual(got.State, tt.want.State) {
+					t.Errorf("NewPlatform().State = %+v, want %+v", got.State, tt.want.State)
+				}
+				if !reflect.DeepEqual(got.Hooks, tt.want.Hooks) {
+					t.Errorf("NewPlatform().Hooks = %+v, want %+v", got.Hooks, tt.want.Hooks)
+				}
+				if !reflect.DeepEqual(got.LogMiddleware, tt.want.LogMiddleware) {
+					t.Errorf("NewPlatform().LogMiddleware = %+v, want %+v", got.LogMiddleware, tt.want.LogMiddleware)
+				}
+			}
+		})
+	}
+}
 
 func TestPlatform_BindVars(t *testing.T) {
 	tests := []struct {
