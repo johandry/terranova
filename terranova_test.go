26e2633b314e43b46553fc5b5cca347bedd2a677
Johandry Amador <johandry@gmail.com>
2020-01-09 12:02:53 -0800

    Apply() test & ignore vscode files

--- terranova_test.go
+++ terranova_test.go
@@ -24,10 +24,12 @@ import (
 	"reflect"
 	"strings"
 	"testing"
+
+	"github.com/hashicorp/terraform/terraform"
 )
 
 func TestPlatform_AddFile_saveCode(t *testing.T) {
-	tmpDir, err := ioutil.TempDir("", ".terranova-test")
+	tmpDir, err := ioutil.TempDir("", ".terranova_test")
 	if err != nil {
 		t.Errorf("Failed to create temporal directory. %s", err)
 	}
@@ -143,3 +145,58 @@ func getSavedFiles(cfgPath string) (map[string]string, error) {
 
 	return got, err
 }
+
+func TestPlatform_Apply(t *testing.T) {
+	type fields struct {
+		Code      string
+		CodeFiles map[string]string
+		Vars      map[string]interface{}
+		Providers map[string]terraform.ResourceProvider
+		State     *State
+		Hooks     []terraform.Hook
+	}
+
+	tests := []struct {
+		name    string
+		fields  fields
+		destroy bool
+		wantErr bool
+	}{
+		{"null data source", fields{Code: nullDataSource}, false, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			p := NewPlatform(tt.fields.Code, tt.fields.Hooks...).BindVars(tt.fields.Vars)
+			if tt.fields.State != nil {
+				p.State = tt.fields.State
+			}
+			for filename, code := range tt.fields.CodeFiles {
+				p.AddFile(filename, code)
+			}
+
+			for pName, provider := range tt.fields.Providers {
+				p.AddProvider(pName, provider)
+			}
+
+			if err := p.Apply(tt.destroy); (err != nil) != tt.wantErr {
+				t.Errorf("Platform.Apply() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+const nullDataSource = `data "null_data_source" "values" {
+  inputs = {
+    all_server_ids = "foo"
+    all_server_ips = "bar"
+  }
+}
+
+output "all_server_ids" {
+  value = "${data.null_data_source.values.outputs["all_server_ids"]}"
+}
+
+output "all_server_ips" {
+  value = "${data.null_data_source.values.outputs["all_server_ips"]}"
+}
+`
